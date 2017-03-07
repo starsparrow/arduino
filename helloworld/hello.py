10a3b6de15d40e1fe3520d33566590a0c194b5ff
starsparrow <nik.spurgetis@gmail.com>
2017-03-06 22:38:26 -0500

    Oooooooh, an arduino-python thing

--- helloworld/hello.py
+++ helloworld/hello.py
@@ -2,7 +2,7 @@ import serial
 import time
 ser = serial.Serial('COM3', 9600, timeout=0)
 
-while 1:
+while True:
     try:
         print(ser.readline())
         time.sleep(1)
--- weather/fetch_weather.py
+++ weather/fetch_weather.py
@@ -1,10 +1,14 @@
 #! /usr/bin/env python
 
-import json, requests
+# Tell your Arduino the weather forecast!
 
-url = 'http://forecast.weather.gov/MapClick.php?lat=39.173&lon=-86.5298&unit=0&lg=english&FcstType=json'
+import json, requests, time
+from arduino import Arduino
 
-forecast = requests.get(url).json()
+URL = 'http://forecast.weather.gov/MapClick.php?lat=39.173&lon=-86.5298&unit=0&lg=english&FcstType=json'
+OUTPUT = '/dev/ttyACM0' # Change as needed
+
+forecast = requests.get(URL).json()
 
 print('{} Weather'.format(
 	forecast['location']['areaDescription']))
@@ -12,3 +16,17 @@ print('Temp: {}F'.format(
 	forecast['currentobservation']['Temp']))
 print('Current Conditions: {}'.format(
 	forecast['currentobservation']['Weather']))
+
+board = Arduino(OUTPUT)
+pin = 13
+
+board.output([pin])
+
+# Blink the LED as many times as the current temp
+for x in range(1, int(forecast['currentobservation']['Temp'])):
+	board.setHigh(pin)
+	time.sleep(0.2)
+	board.setLow(pin)
+	time.sleep(0.2)
+
+board.close()
