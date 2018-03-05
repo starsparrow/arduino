614f10f1c09ad3a226791ca708f3f245512a7a6e
starsparrow <nik.spurgetis@gmail.com>
2018-03-05 16:17:47 -0500

    Added RGB LED interface

---
+++ interfaces/rgb_led/rgb_led/rgb_led.ino
@@ -0,0 +1,28 @@
+int redPin = 9;
+int greenPin = 10;
+int bluePin = 11;
+int delayDuration = 500; // ms
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+   setColor(0xFF,0x0,0x0);
+}
+
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+  delay(delayDuration);
+}
+
