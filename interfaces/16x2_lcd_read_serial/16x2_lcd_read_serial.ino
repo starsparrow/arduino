e689587435db0fc8112bc9fdd0ce7d7c66eb35cb
starsparrow <nik.spurgetis@gmail.com>
2018-03-05 16:19:18 -0500

    Restructured interface directory

--- interfaces/16x2_lcd_read_serial/16x2_lcd_read_serial.ino
+++ interfaces/16x2_lcd_read_serial/16x2_lcd_read_serial.ino
@@ -1,13 +1,25 @@
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 13, 5, 4, 3, 2);
+
+int redPin = 9;
+int greenPin = 10;
+int bluePin = 11;
+int delayDuration = 500; // ms
+bool lightsEnabled = true;
+
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // initialize the serial communications:
   Serial.begin(9600);
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
 }
 
 void loop() {
@@ -35,4 +47,21 @@ void loop() {
         }
       }
   }
+  if (lightsEnabled) {
+   setColor(0x0,0x0,0xFF);
+   }
 }
+
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+  delay(delayDuration);
+}
+
